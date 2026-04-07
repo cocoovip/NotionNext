7f43619b809e2a097ddc940b10c3601d7cc9bcc7
cocoovip <167426639+cocoovip@users.noreply.github.com>
2026-04-07 16:52:46 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,69 +1,12 @@
-import { siteConfig } from '@/lib/config'
-import { useGlobal } from '@/lib/global'
-import SmartLink from '@/components/SmartLink'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import CONFIG from '../config'
-import NotByAI from '@/components/NotByAI'
-
 /**
- * 版权声明
- * @returns
- */
-export default function PostCopyright() {
-  const router = useRouter()
-  const [path, setPath] = useState(siteConfig('LINK') + router.asPath)
-  useEffect(() => {
-    setPath(window.location.href)
-  })
-
-  const { locale } = useGlobal()
-
-  if (!siteConfig('HEO_ARTICLE_COPYRIGHT', null, CONFIG)) {
-    return <></>
-  }
-
-  return (
-    <section className='dark:text-gray-300 mt-6 mx-1 '>
-      <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
-        <li>
-          <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
-          <SmartLink href={'/about'} className='hover:underline'>
-            {siteConfig('AUTHOR')}
-          </SmartLink>
-        </li>
-        <li>
-          <strong className='mr-2'>{locale.COMMON.URL}:</strong>
-          <a
-            className='whitespace-normal break-words hover:underline'
-            href={path}>
-            {path}
-          </a>
-        </li>
-        <li>
-          <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
-          {locale.COMMON.COPYRIGHT_NOTICE}
-        </li>
-        {siteConfig('HEO_ARTICLE_NOT_BY_AI', false, CONFIG) && (
-          <li>
-            <NotByAI />
-          </li>
-        )}
-      </ul>
-    </section>
-  )
-}
-
-/**
- * 页脚
+ * 自定义页脚（已去除 NotionNext 痕迹）
  */
 const Footer = () => {
   return (
-    <footer className="w-full h-20 flex flex-col justify-between items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F] p-6">
-      <div className="text-center">
-        {/* 在这里放置你自己的内容，删除 "Powered by NotionNext" */}
-        <p>Powered by qzio</p>
-        <p>© 2026 qzio. All rights reserved.</p>
+    <footer className="w-full flex flex-col justify-center items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F] p-6">
+      <div className="text-center text-sm text-gray-500 dark:text-gray-400">
+        <p>© 2026 qzio</p>
+        <p>All rights reserved.</p>
       </div>
     </footer>
   )
