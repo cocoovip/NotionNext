bb1e3cd066586e4b0da5c0b9f32a3ca0566b60f3
cocoovip <167426639+cocoovip@users.noreply.github.com>
2026-04-07 17:08:55 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -95,7 +95,7 @@ const LayoutBase = props => {
   return (
     <div
       id='theme-heo'
-      className={`${siteConfig('FONT_STYLE')} bg-[#f7f9fe] dark:bg-[#18171d] h-full min-h-screen flex flex-col scroll-smooth`}>
+      className={`${siteConfig('FONT_STYLE')} bg-[#f7f9fe] dark:bg-[#18171d] min-h-screen flex flex-col scroll-smooth`}>
       <Style />
 
       {/* 顶部嵌入 导航栏，首页放hero，文章页放文章详情 */}
@@ -124,7 +124,9 @@ const LayoutBase = props => {
       </main>
 
       {/* 页脚 */}
-      <Footer />
+      <div className="mt-auto">
+  <Footer />
+</div>
 
       {HEO_LOADING_COVER && <LoadingCover />}
     </div>
