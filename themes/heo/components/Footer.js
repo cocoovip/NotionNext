4aed491288d3c165c2803718baac783dcf0dd64a
cocoovip <167426639+cocoovip@users.noreply.github.com>
2026-04-07 16:29:40 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,70 +1,69 @@
-import { BeiAnGongAn } from '@/components/BeiAnGongAn'
-import CopyRightDate from '@/components/CopyRightDate'
-import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
-import SocialButton from './SocialButton'
+import { useGlobal } from '@/lib/global'
+import SmartLink from '@/components/SmartLink'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import CONFIG from '../config'
+import NotByAI from '@/components/NotByAI'
+
 /**
- * 页脚
+ * 版权声明
  * @returns
  */
-const Footer = () => {
-  const BEI_AN = siteConfig('BEI_AN')
-  const BEI_AN_LINK = siteConfig('BEI_AN_LINK')
-  const BIO = siteConfig('BIO')
+export default function PostCopyright() {
+  const router = useRouter()
+  const [path, setPath] = useState(siteConfig('LINK') + router.asPath)
+  useEffect(() => {
+    setPath(window.location.href)
+  })
+
+  const { locale } = useGlobal()
+
+  if (!siteConfig('HEO_ARTICLE_COPYRIGHT', null, CONFIG)) {
+    return <></>
+  }
+
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
-      {/* 颜色过度区 */}
-      <div
-        id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
-      />
+    <section className='dark:text-gray-300 mt-6 mx-1 '>
+      <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
+        <li>
+          <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
+          <SmartLink href={'/about'} className='hover:underline'>
+            {siteConfig('AUTHOR')}
+          </SmartLink>
+        </li>
+        <li>
+          <strong className='mr-2'>{locale.COMMON.URL}:</strong>
+          <a
+            className='whitespace-normal break-words hover:underline'
+            href={path}>
+            {path}
+          </a>
+        </li>
+        <li>
+          <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
+          {locale.COMMON.COPYRIGHT_NOTICE}
+        </li>
+        {siteConfig('HEO_ARTICLE_NOT_BY_AI', false, CONFIG) && (
+          <li>
+            <NotByAI />
+          </li>
+        )}
+      </ul>
+    </section>
+  )
+}
 
-      {/* 社交按钮 */}
-      <div className='w-full h-24'>
-        <SocialButton />
-      </div>
-
-      <br />
-
-      {/* 底部页面信息 */}
-      <div
-        id='footer-bottom'
-        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
-        <div id='footer-bottom-left' className='text-center lg:text-start'>
-          <div>测试 Footer</div>
-          <div className='flex gap-x-1'>
-            <CopyRightDate />
-            <a
-              href={'/'}
-              className='underline font-semibold dark:text-gray-300 '>
-              {siteConfig('AUTHOR')}
-            </a>
-            {BIO && <span className='mx-1'> | {BIO}</span>}
-          </div>
-        </div>
-
-        <div id='footer-bottom-right'>
-          {BEI_AN && (
-            <>
-              <i className='fas fa-shield-alt' />{' '}
-              <a href={BEI_AN_LINK} className='mr-2'>
-                {siteConfig('BEI_AN')}
-              </a>
-            </>
-          )}
-          <BeiAnGongAn />
-
-          <span className='hidden busuanzi_container_site_pv'>
-            <i className='fas fa-eye' />
-            <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-          </span>
-          <span className='pl-2 hidden busuanzi_container_site_uv'>
-            <i className='fas fa-users' />{' '}
-            <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-          </span>
-
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
-        </div>
+/**
+ * 页脚
+ */
+const Footer = () => {
+  return (
+    <footer className="w-full h-20 flex flex-col justify-between items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F] p-6">
+      <div className="text-center">
+        {/* 在这里放置你自己的内容，删除 "Powered by NotionNext" */}
+        <p>Powered by qzio</p>
+        <p>© 2026 qzio. All rights reserved.</p>
       </div>
     </footer>
   )
