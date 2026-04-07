591e3d8d1284402fd4878013fe67bfe79f9c08da
cocoovip <167426639+cocoovip@users.noreply.github.com>
2026-04-07 12:38:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,8 +19,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '平面设计', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '深耕平面设计领域', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://qzio.qzz.io/', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '设计, 创意，视觉，教程，视频', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
